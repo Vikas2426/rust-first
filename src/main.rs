6dbd94b9eb782b3c38155a787b56c7a1702df9f5
Vikas2426 <vikas.cby@gmail.com>
2024-02-28 01:39:50 +0530

    let and const

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 4;
+    println!("x={} x+1={}", x, x + 1);
+    x = x + 1;
+    {
+        let x = 2;
+        println!("x={} x+1={}", x, x + 1);
+    }
+    let x = x + 1;
+    println!("x={} x+1={}", x, x + 1);
+
+    let x = "hello";
+    println!("x={}", x);
+
+    const MY_CONST: u32 = 60;
+    println!("my const={}", MY_CONST);
 }
